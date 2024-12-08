8a7f581d78755593d74bd4ca1a21a18c0b5333c1
Dagar Rehan <dagar.rehan@gmail.com>
2024-12-08 14:23:53 -0500

    Modified

--- MLP.py
+++ MLP.py
@@ -3,17 +3,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MLP(nn.Module):
-    def __init__(self, input_size=1024, output_size=4):
+    def __init__(self, input_size=1024, output_size=4, dropout_rate=0.35):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, 128)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(128, output_size)
+        self.fc1 = nn.Linear(input_size, 512)
+        self.lrelu1 = nn.LeakyReLU()
+        self.dropout1 = nn.Dropout(dropout_rate)
+        
+        self.fc2 = nn.Linear(512, 128)
+        self.lrelu2 = nn.LeakyReLU()
+        self.dropout2 = nn.Dropout(dropout_rate)
+
+        self.fc3 = nn.Linear(128, output_size)
+
         self.softmax = nn.Softmax(dim = 1)
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.relu(x)
+        x = self.lrelu1(x)
+        x = self.dropout1(x)
+
         x = self.fc2(x)
+        x = self.lrelu2(x)
+        x = self.dropout2(x)
+
+        x = self.fc3(x)
+
         x = self.softmax(x)
         return x
 
